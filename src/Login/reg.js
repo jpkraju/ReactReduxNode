1ad9e5c885462dc202947f82bac66d5d0630d1c5
Raju <jp.k.raju@gmail.com>
2024-04-30 17:20:25 +0530

    gender select ddl changes

--- src/Login/reg.js
+++ src/Login/reg.js
@@ -3,13 +3,13 @@ import React, { useState } from "react";
 import { validateSubmit } from "../utils/loginVerification";
 
 function NewRegistration(props) {
-
-  const [firstName, setfirstName] = useState('');
-  const [lastName, setlastName] = useState('');
-  const [email, setEmail] = useState('');
-  const [mobile, setMobile] = useState('');
-  const [password, setPassword] = useState('');
-  const [retypePassword, setRetypePassword] = useState('');
+  const [firstName, setfirstName] = useState("");
+  const [lastName, setlastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [mobile, setMobile] = useState("");
+  const [password, setPassword] = useState("");
+  const [retypePassword, setRetypePassword] = useState("");
+  const [gender, setGender] = useState("male");
 
   function backbuttonclickhandler(e) {
     e.preventDefault();
@@ -54,51 +54,124 @@ function NewRegistration(props) {
     const value = target.value;
     setRetypePassword(value);
   }
-  
+
   function submitButtonClickHandler(e) {
     e.preventDefault();
-    validateSubmit(firstName, lastName, email, mobile, password, retypePassword);
+    validateSubmit(
+      firstName,
+      lastName,
+      email,
+      mobile,
+      password,
+      retypePassword,
+      gender
+    );
   }
+
+
+
+  const handleSelectChange = (event) => {
+    event.preventDefault();
+    setGender(event.target.value);
+  };
 
   return (
     <div className="login">
-      <label><b>First Name</b></label>
-      <input type="text" name="Uname" id="Uname" placeholder="First Name" onChange={firstNameChange} />
+      <label>
+        <b>First Name</b>
+      </label>
+      <input
+        type="text"
+        name="Uname"
+        id="Uname"
+        placeholder="First Name"
+        onChange={firstNameChange}
+      />
       <br />
       <br />
-      <label><b>Last Name</b></label>
-      <input type="text" name="Pass" id="Pass" placeholder="Last Name" onChange={lastNameChange} />
+      <label>
+        <b>Last Name</b>
+      </label>
+      <input
+        type="text"
+        name="Pass"
+        id="Pass"
+        placeholder="Last Name"
+        onChange={lastNameChange}
+      />
       <br />
       <br />
-      <label><b>Email@gmail</b></label>
-      <input type="text" name="Uname" id="Uname" placeholder="Email" onChange={emailChange} />
+      <label>
+        <b>Email@gmail</b>
+      </label>
+      <input
+        type="text"
+        name="Uname"
+        id="Uname"
+        placeholder="Email"
+        onChange={emailChange}
+      />
       <br />
       <br />
-      <label><b>Mobile No</b></label>
-      <input type="text" name="Uname" id="Uname" placeholder="Mobile No" onChange={mobileNoChange} />
+      <label>
+        <b>Mobile No</b>
+      </label>
+      <input
+        type="text"
+        name="Uname"
+        id="Uname"
+        placeholder="Mobile No"
+        onChange={mobileNoChange}
+      />
       <br />
       <br />
-      <label><b>Password</b></label>
-      <input type="Password" name="Pass" id="Pass" placeholder="Password" onChange={passwordChange} />
+      <label>
+        <b>Password</b>
+      </label>
+      <input
+        type="Password"
+        name="Pass"
+        id="Pass"
+        placeholder="Password"
+        onChange={passwordChange}
+      />
       <br />
       <br />
-      <label><b>Re-type Password</b></label>
-      <input type="Password" name="Pass" id="Pass" placeholder="Re-type Password" onChange={retypePasswordChange} />
+      <label>
+        <b>Re-type Password</b>
+      </label>
+      <input
+        type="Password"
+        name="Pass"
+        id="Pass"
+        placeholder="Re-type Password"
+        onChange={retypePasswordChange}
+      />
       <br />
       <br />
       <label for="gender">Select Gender</label>
-      <select id="gender" name="gender" required>
+      <select value={gender} onChange={handleSelectChange}>
         <option value="male">Male</option>
+
         <option value="female">Female</option>
-        <option value="other">Other</option>
       </select>
       <br />
       <br />
-      <input type="button" name="log" id="log" value="Register" onClick={submitButtonClickHandler} />
+      <input
+        type="button"
+        name="log"
+        id="log"
+        value="Register"
+        onClick={submitButtonClickHandler}
+      />
       <br />
       <br />
       <input
-        type="button" name="back" id="back" value="Back" onClick={backbuttonclickhandler}
+        type="button"
+        name="back"
+        id="back"
+        value="Back"
+        onClick={backbuttonclickhandler}
       />
     </div>
   );
--- src/utils/loginVerification.js
+++ src/utils/loginVerification.js
@@ -1,50 +1,58 @@
-import users from "./users.json"
+import users from "./users.json";
 export function validateLogin(username, password) {
-    console.log('>>> ', username, password);
+  console.log(">>> ", username, password);
 
-    var user = {
-        user_id: "",
-        first_name: "",
-        last_name: "",
-        email_id: "",
-        phone: "",
-        error: "",
-    }
+  var user = {
+    user_id: "",
+    first_name: "",
+    last_name: "",
+    email_id: "",
+    phone: "",
+    error: "",
+  };
 
-    var userobj=users.find((x)=>x.username===username)
-    if(userobj!==undefined ){
-        if(userobj.password===password){
-            user.user_id=userobj.user_id;
-            user.first_name=userobj.first_name;
-            user.last_name=userobj.last_name;
-            user.email_id = userobj.email_id;
-            user.phone = userobj.phone;            
-        }
-        else{
-            user.error="Incorrect Password";
-        }
+  var userobj = users.find((x) => x.username === username);
+  if (userobj !== undefined) {
+    if (userobj.password === password) {
+      user.user_id = userobj.user_id;
+      user.first_name = userobj.first_name;
+      user.last_name = userobj.last_name;
+      user.email_id = userobj.email_id;
+      user.phone = userobj.phone;
+    } else {
+      user.error = "Incorrect Password";
     }
-    else{
-        user.error="user " +username+ " does not exist!!";
-    }
-    console.log(user.error);
-    return user;
+  } else {
+    user.error = "user " + username + " does not exist!!";
+  }
+  console.log(user.error);
+  return user;
 }
-export function validateSubmit(firstName,lastName,email,mobile,password, retypePassword){
-    var user = {
-        first_name: "",
-        last_name: "",
-        Email:"",
-        Mobile:"",
-        Password:"",
-        RetypePassword:"",
-        error: "",
-    }
-    user.first_name=firstName;
-    user.last_name= lastName;
-    user.Email=email;
-    user.Mobile=mobile;
-    user.Password=password;
-    user.RetypePassword=retypePassword;
-    console.log(user);
+export function validateSubmit(
+  firstName,
+  lastName,
+  email,
+  mobile,
+  password,
+  retypePassword,
+  gender
+) {
+  var user = {
+    first_name: "",
+    last_name: "",
+    Email: "",
+    Mobile: "",
+    Password: "",
+    RetypePassword: "",
+    gender: "",
+    error: "",
+  };
+  user.first_name = firstName;
+  user.last_name = lastName;
+  user.Email = email;
+  user.Mobile = mobile;
+  user.Password = password;
+  user.RetypePassword = retypePassword;
+  user.gender = gender;
+  console.log(user);
 }

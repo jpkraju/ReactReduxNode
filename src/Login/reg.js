18f7ec295ce677f997b18384d09c10207f4fa36d
kanchankasniya <kanchankasniya@gmail.com>
2024-04-30 15:23:43 +0530

    registration updated

--- src/Login/reg.js
+++ src/Login/reg.js
@@ -1,54 +1,89 @@
 import "./login.css";
+import React, { useState } from "react";
+import { validateSubmit } from "../utils/loginVerification";
 
 function NewRegistration(props) {
+
+  const [firstName, setfirstName] = useState('');
+  const [lastName, setlastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [mobile, setMobile] = useState('');
+  const [password, setPassword] = useState('');
+  const [retypePassword, setRetypePassword] = useState('');
+
   function backbuttonclickhandler(e) {
     e.preventDefault();
     // setPageno(0)
     props.loginBackButtonHandler();
   }
 
+  function firstNameChange(e) {
+    e.preventDefault();
+    const target = e.target;
+    const value = target.value;
+    setfirstName(value);
+  }
+  function lastNameChange(e) {
+    e.preventDefault();
+    const target = e.target;
+    const value = target.value;
+    setlastName(value);
+  }
+  function emailChange(e) {
+    e.preventDefault();
+    const target = e.target;
+    const value = target.value;
+    setEmail(value);
+  }
+  function mobileNoChange(e) {
+    e.preventDefault();
+    const target = e.target;
+    const value = target.value;
+    setMobile(value);
+  }
+
+  function passwordChange(e) {
+    e.preventDefault();
+    const target = e.target;
+    const value = target.value;
+    setPassword(value);
+  }
+  function retypePasswordChange(e) {
+    e.preventDefault();
+    const target = e.target;
+    const value = target.value;
+    setRetypePassword(value);
+  }
+  
+  function submitButtonClickHandler(e) {
+    e.preventDefault();
+    validateSubmit(firstName, lastName, email, mobile, password, retypePassword);
+  }
+
   return (
     <div className="login">
-      <label>
-        <b>First Name</b>
-      </label>
-      <input type="text" name="Uname" id="Uname" placeholder="First Name" />
+      <label><b>First Name</b></label>
+      <input type="text" name="Uname" id="Uname" placeholder="First Name" onChange={firstNameChange} />
       <br />
       <br />
-      <label>
-        <b>Last Name</b>
-      </label>
-      <input type="text" name="Pass" id="Pass" placeholder="Last Name" />
+      <label><b>Last Name</b></label>
+      <input type="text" name="Pass" id="Pass" placeholder="Last Name" onChange={lastNameChange} />
       <br />
       <br />
-      <label>
-        <b>Email@gmail</b>
-      </label>
-      <input type="text" name="Uname" id="Uname" placeholder="Email" />
+      <label><b>Email@gmail</b></label>
+      <input type="text" name="Uname" id="Uname" placeholder="Email" onChange={emailChange} />
       <br />
       <br />
-      <label>
-        <b>Mobile No</b>
-      </label>
-      <input type="text" name="Uname" id="Uname" placeholder="Mobile No" />
+      <label><b>Mobile No</b></label>
+      <input type="text" name="Uname" id="Uname" placeholder="Mobile No" onChange={mobileNoChange} />
       <br />
       <br />
-
-      <label>
-        <b>Password</b>
-      </label>
-      <input type="Password" name="Pass" id="Pass" placeholder="Password" />
+      <label><b>Password</b></label>
+      <input type="Password" name="Pass" id="Pass" placeholder="Password" onChange={passwordChange} />
       <br />
       <br />
-      <label>
-        <b>Re-type Password</b>
-      </label>
-      <input
-        type="Password"
-        name="Pass"
-        id="Pass"
-        placeholder="Re-type Password"
-      />
+      <label><b>Re-type Password</b></label>
+      <input type="Password" name="Pass" id="Pass" placeholder="Re-type Password" onChange={retypePasswordChange} />
       <br />
       <br />
       <label for="gender">Select Gender</label>
@@ -59,16 +94,11 @@ function NewRegistration(props) {
       </select>
       <br />
       <br />
-
-      <input type="button" name="log" id="log" value="Register" />
+      <input type="button" name="log" id="log" value="Register" onClick={submitButtonClickHandler} />
       <br />
       <br />
       <input
-        type="button"
-        name="back"
-        id="back"
-        value="Back"
-        onClick={backbuttonclickhandler}
+        type="button" name="back" id="back" value="Back" onClick={backbuttonclickhandler}
       />
     </div>
   );
